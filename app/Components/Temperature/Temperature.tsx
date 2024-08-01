6581081d179e474c8633aabf78659dcdc1b14483
radhaya9av <radhaya9av@gmail.com>
2024-08-01 12:22:48 +0530

    tempratureComponent

---
+++ app/Components/Temperature/Temperature.tsx
@@ -0,0 +1,93 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useGlobalContext } from "@/app/context/globalContext";
+import {
+  clearSky,
+  cloudy,
+  drizzleIcon,
+  navigation,
+  rain,
+  snow,
+} from "@/app/utils/Icons";
+import moment from "moment";
+
+function Temperature() {
+  const { forecast } = useGlobalContext();
+
+  const { main, timezone, name, weather } = forecast;
+
+  if (!forecast || !weather) {
+    return <div>Loading...</div>;
+  }
+
+  // State
+  const [localTime, setLocalTime] = useState<string>("");
+  const [currentDay, setCurrentDay] = useState<string>("");
+
+  const { main: weatherMain, description } = weather[0];
+
+  const getIcon = () => {
+    switch (weatherMain) {
+      case "Drizzle":
+        return drizzleIcon;
+      case "Rain":
+        return rain;
+      case "Snow":
+        return snow;
+      case "Clear":
+        return clearSky;
+      case "Clouds":
+        return cloudy;
+      default:
+        return clearSky;
+    }
+  };
+
+  // Live time update
+  useEffect(() => {
+    // upadte time every second
+    const interval = setInterval(() => {
+      const localMoment = moment().utcOffset(timezone / 60);
+      // custom format: 24 hour format
+      const formatedTime = localMoment.format("HH:mm:ss");
+      // day of the week
+      const day = localMoment.format("dddd");
+
+      setLocalTime(formatedTime);
+      setCurrentDay(day);
+    }, 1000);
+
+    // clear interval
+    return () => clearInterval(interval);
+  }, [timezone]);
+
+  return (
+    <div
+      className="pt-6 pb-5 px-4 border rounded-lg flex flex-col 
+        justify-between dark:bg-dark-grey shadow-sm dark:shadow-none"
+    >
+      <p className="flex justify-between items-center">
+        <span className="font-medium">{currentDay}</span>
+        <span className="font-medium">{localTime}</span>
+      </p>
+      <p className="pt-2 font-bold flex gap-1">
+        <span>{name}</span>
+        <span>{navigation}</span>
+      </p>
+      <p className="py-10 text-9xl font-bold self-center">{}°</p>
+
+      <div>
+        <div>
+          <span>{getIcon()}</span>
+          <p className="pt-2 capitalize text-lg font-medium">{description}</p>
+        </div>
+        <p className="flex items-center gap-2">
+          <span>Low: {0}°</span>
+          <span>High: {1}°</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Temperature;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Navbar from "./Components/Navbar";
+import Temperature from "./Components/Temperature/Temperature";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Temperature />
     </main>
   );
 }
