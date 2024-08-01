42d83f04213c3cb3221ab626b09df2d4b16ed357
radhaya9av <radhaya9av@gmail.com>
2024-08-01 12:09:02 +0530

    weatherAPIfetch

--- app/context/globalContext.js
+++ app/context/globalContext.js
@@ -1,4 +1,5 @@
 "use Client";
+import axios from "axios";
 import React, { useContext, createContext, useState, useEffect } from "react";
 import defaultStates from "../utils/defaultStates";
 
@@ -19,6 +20,68 @@ export const GlobalContextProvider = ({ children }) => {
   const [airQuality, setAirQuality] = useState({});
   const [fiveDayForecast, setFiveDayForecast] = useState({});
   const [uvIndex, seUvIndex] = useState({});
+
+  const fetchForecast = async (lat, lon) => {
+    try {
+      const res = await axios.get(`api/weather?lat=${lat}&lon=${lon}`);
+
+      setForecast(res.data);
+    } catch (error) {
+      console.log("Error fetching forecast data: ", error.message);
+    }
+  };
+
+  // Air Quality
+  const fetchAirQuality = async (lat, lon) => {
+    try {
+      const res = await axios.get(`api/pollution?lat=${lat}&lon=${lon}`);
+      setAirQuality(res.data);
+    } catch (error) {
+      console.log("Error fetching air quality data: ", error.message);
+    }
+  };
+
+  // five day forecast
+  const fetchFiveDayForecast = async (lat, lon) => {
+    try {
+      const res = await axios.get(`api/fiveday?lat=${lat}&lon=${lon}`);
+
+      setFiveDayForecast(res.data);
+    } catch (error) {
+      console.log("Error fetching five day forecast data: ", error.message);
+    }
+  };
+
+  //geocoded list
+  const fetchGeoCodedList = async (search) => {
+    try {
+      const res = await axios.get(`/api/geocoded?search=${search}`);
+
+      setGeoCodedList(res.data);
+    } catch (error) {
+      console.log("Error fetching geocoded list: ", error.message);
+    }
+  };
+
+  //fetch uv data
+  const fetchUvIndex = async (lat, lon) => {
+    try {
+      const res = await axios.get(`/api/uv?lat=${lat}&lon=${lon}`);
+
+      seUvIndex(res.data);
+    } catch (error) {
+      console.error("Error fetching the forecast:", error);
+    }
+  };
+
+  // handle input
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+
+    if (e.target.value === "") {
+      setGeoCodedList(defaultStates);
+    }
+  };
 
   // debounce function
   useEffect(() => {
