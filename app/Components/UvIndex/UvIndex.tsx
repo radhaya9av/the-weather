33365289cc0194560d3fb378498e60dc4307e399
radhaya9av <radhaya9av@gmail.com>
2024-08-01 16:43:15 +0530

    uvindexAPI

--- app/Components/UvIndex/UvIndex.tsx
+++ app/Components/UvIndex/UvIndex.tsx
@@ -18,6 +18,40 @@ function UvIndex() {
 
   const uvIndexMax = uv_index_max[0].toFixed(0);
 
+  const uvIndexCategory = (uvIndex: number) => {
+    if (uvIndex <= 2) {
+      return {
+        text: "Low",
+        protection: "No protection required",
+      };
+    } else if (uvIndex <= 5) {
+      return {
+        text: "Moderate",
+        protection: "Stay in shade near midday.",
+      };
+    } else if (uvIndex <= 7) {
+      return {
+        text: "High",
+        protection: "Wear a hat and sunglasses.",
+      };
+    } else if (uvIndex <= 10) {
+      return {
+        text: "Very High",
+        protection: "Apply sunscreen SPF 30+ every 2 hours.",
+      };
+    } else if (uvIndex > 10) {
+      return {
+        text: "Extreme",
+        protection: "Avoid being outside.",
+      };
+    } else {
+      return {
+        text: "Extreme",
+        protection: "Avoid being outside.",
+      };
+    }
+  };
+
   const marginLeftPercentage = (uvIndexMax / 14) * 100;
 
   return (
